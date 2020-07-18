5faa5e244932332a0ae6ce83380eaa14b8784c1f
netveldi <netveldi@protonmail.com>
2020-07-18 17:44:38 -0500

    .

---
+++ rock-paper-scissors/precache-manifest.a281975c0112cf422f40be4e384cdfd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ab77a27d1318a559b2a0af2cee2e0c",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "3bd37ce3b101215251dc",
+    "url": "/rock-paper-scissors/static/css/2.6978da55.chunk.css"
+  },
+  {
+    "revision": "7006148ede41adea1e81",
+    "url": "/rock-paper-scissors/static/css/main.4e00193a.chunk.css"
+  },
+  {
+    "revision": "3bd37ce3b101215251dc",
+    "url": "/rock-paper-scissors/static/js/2.bb05109e.chunk.js"
+  },
+  {
+    "revision": "7006148ede41adea1e81",
+    "url": "/rock-paper-scissors/static/js/main.890b50ff.chunk.js"
+  },
+  {
+    "revision": "72068078e48c98f10475",
+    "url": "/rock-paper-scissors/static/js/runtime-main.ce184a6c.js"
+  }
+]);
